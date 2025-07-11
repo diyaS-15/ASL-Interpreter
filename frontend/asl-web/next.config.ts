8ffefa79c50fb7e50cc52337dcb7c611300ff00c
diyaS-15 <122123763+diyaS-15@users.noreply.github.com>
2025-07-10 20:58:31 -0400

    Update next.config.ts

--- frontend/asl-web/next.config.ts
+++ frontend/asl-web/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
+
